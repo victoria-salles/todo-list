d26290f2eeaf5d138589a12f26c0dbe27220eba9
Victoria <victoriasalles360@gmail.com>
2022-12-28 22:58:09 -0300

    second todo-list commit

--- app.js
+++ app.js
@@ -1,2 +1,56 @@
 'use strict';
 
+let database = [
+    {'assingment' : 'Estudar JS', 'status' : ''}
+]
+
+const createItem = (assingment, status, index) => {
+    const item = document.createElement('label');
+    item.classList.add('todo__item');
+    item.innerHTML = `
+        <input type="checkbox" ${status} data-index=${index}>
+        <div>${assingment}</div>
+        <input type="button" value="X" data-index=${index}>
+    `
+    document.getElementById('todoList').appendChild(item);
+}
+
+const clearAssingments = () => {
+    const todoList = document.getElementById('todoList');
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild);
+    }
+}
+
+const attScreen = () => {
+    clearAssingments();
+    database.forEach((item, index) => createItem(item.assingment, item.status, index));
+}
+
+const insertItem = (event) => {
+    const key = event.key;
+    const text = event.target.value;
+    if(key === 'Enter') {
+        database.push({'assingment' : text, 'status' : ''})
+        attScreen();
+        event.target.value = '';
+    }
+}
+
+const removeItem = (index) => {
+    database.splice(index, 1);
+    attScreen();
+}
+
+const clickItem = (event) => {
+    const element = event.target;
+    if(element.type === 'button') {
+        const index = element.dataset.index;
+        removeItem(index);
+    }
+}
+
+document.getElementById('newItem').addEventListener('keypress', insertItem);
+document.getElementById('todoList').addEventListener('click', clickItem);
+
+attScreen();
